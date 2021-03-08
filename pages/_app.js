20f2ac484a4a61315945f70b2d174dd818864ac3
Connor Gunderson <gundydev@gmail.com>
2021-03-08 15:01:07 -0500

    app fix

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,12 @@ import { NextSeo } from 'next-seo';
 
 import SEO from '../next-seo.config';
 import { AuthProvider } from '@/lib/auth';
-import { CardProvider } from '@/utils/cards';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <CardProvider>
         <NextSeo {...SEO} />
         <Component {...pageProps} />
-      </CardProvider>
     </AuthProvider>
   );
 }
